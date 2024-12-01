07287595595a291a48272a161dddccff9ceded9f
druap <dev1lc3st@gmail.com>
2024-12-01 18:54:04 +0300

    fixing main backup point

--- tests/test_s21_main.c
+++ tests/test_s21_main.c
@@ -10,26 +10,15 @@ Suite *s21_memcmp_suite(void);
 int main(void) {
   int number_failed;
 
-  // Создаем SRunner для запуска тестов
-  SRunner *sr =
-      srunner_create(s21_memcmp_suite());  // Добавляем набор тестов s21_memcmp
+  SRunner *sr = srunner_create(s21_memcmp_suite());
 
-  // Добавляем другие тесты в SRunner
-  srunner_add_suite(sr,
-                    s21_memchr_suite());  // Добавляем набор тестов s21_memchr
-  // srunner_add_suite(sr, другой_набор_тестов()); // Добавьте другие тесты,
-  // если они есть
+  srunner_add_suite(sr, s21_memchr_suite());
 
-  // Запуск всех тестов
-  srunner_run_all(
-      sr, CK_VERBOSE);  // Используем CK_VERBOSE для детализированного вывода
+  srunner_run_all(sr, CK_NORMAL);
 
-  // Получаем количество неудачных тестов
   number_failed = srunner_ntests_failed(sr);
 
-  // Освобождаем ресурсы
   srunner_free(sr);
 
-  // Возвращаем 0, если все тесты прошли успешно, иначе 1
   return (number_failed == 0) ? 0 : 1;
 }
